eb02aa56a372a9c3ef4b485e2f7b66fad769e604
Daniel Amenyenu <ameneynudaniel45@gmail.com>
2024-01-18 14:27:05 +0000

    Updates

---
+++ sanity/schema/schema.ts
@@ -0,0 +1,9 @@
+import { type SchemaTypeDefinition } from "sanity";
+
+import post from "./post";
+import user from "./user";
+import comment from "./comment";
+import postedBy from "./postedBy";
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [post, user, comment, postedBy],
+};
